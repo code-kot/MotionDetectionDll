46370c1d398ee708f2ea945790f0ea33b536a0af
code-kot <code-kot@users.noreply.github.com>
2019-07-01 11:28:50 +0300

    Updated exported types.

--- Source/API.h
+++ Source/API.h
@@ -7,17 +7,19 @@
 #define MOTION_DETECTION_API __declspec(dllimport)
 #endif
 
-typedef unsigned long long h_instance;
-typedef void (*callback)(int left, int top /* more params ?? */);
+typedef void* h_instance;
+typedef void (__cdecl *callback)(int left, int top /* more params ?? */);
 
 // create and init instance
-extern "C" MOTION_DETECTION_API h_instance create_instance(callback callback, unsigned long frame_width, unsigned long frame_height/* settings ?? */);
+extern "C" {
+	MOTION_DETECTION_API h_instance create_instance(callback callback, unsigned int frame_width, unsigned int frame_height/* settings ?? */);
 
-// add new frame
-extern "C" MOTION_DETECTION_API void add_frame(h_instance instance, void* pixels, unsigned int bytes_per_line /* frame ?? */);
+	// add new frame
+	MOTION_DETECTION_API void add_frame(h_instance instance, void* pixels, unsigned int bytes_per_line /* frame ?? */);
 
-// reset
-extern "C" MOTION_DETECTION_API void reset_instance(h_instance instance);
+	// reset
+	MOTION_DETECTION_API void reset_instance(h_instance instance);
 
-// delete
-extern "C" MOTION_DETECTION_API void delete_instance(h_instance instance);
+	// delete
+	MOTION_DETECTION_API void delete_instance(h_instance instance);
+}
